45fc851353c7da1c37cab13a730cf9e2c201c1a7
PalvarezLara <pame.alvarez@duocuc.cl>
2025-10-10 00:15:10 -0300

    chore(estructura): completar estructura MVVM con modelos, viewmodels, data y repository base

---
+++ app/src/main/java/com/example/spadelbosque/model/ItemCarrito.kt
@@ -0,0 +1,8 @@
+package com.example.spadelbosque.model
+
+//momentaneo para que no este vacio
+data class ItemCarrito(
+    val id: Int = 0,
+    val nombre: String = "",
+    val correo: String = ""
+)
---
+++ app/src/main/java/com/example/spadelbosque/model/Servicio.kt
@@ -0,0 +1,8 @@
+package com.example.spadelbosque.model
+
+//momentaneo para que no este vacio
+data class Servicio(
+    val sku: Int = 0,
+    val nombre: String = "",
+    val duracion: String = ""
+)
