c91d820443cefc5df9554b26d787560b560c0f5d
PalvarezLara <pame.alvarez@duocuc.cl>
2025-11-01 07:55:40 -0300

    Fix: Detalles ultimo merge
    # Conflicts:
    #	.idea/deploymentTargetSelector.xml
    #	app/src/main/java/com/example/spadelbosque/MainActivity.kt
    #	app/src/main/java/com/example/spadelbosque/navigation/AppNavHost.kt
    #	app/src/main/java/com/example/spadelbosque/ui/screens/contacto/ContactoScreen.kt

--- app/src/main/java/com/example/spadelbosque/MainActivity.kt
+++ app/src/main/java/com/example/spadelbosque/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
