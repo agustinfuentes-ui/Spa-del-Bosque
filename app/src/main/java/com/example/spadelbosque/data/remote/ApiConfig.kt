06eeab2e5a7a1e8692443cac49e4f8c8b568194e
PalvarezLara <pame.alvarez@duocuc.cl>
2025-11-28 16:45:36 -0300

    Fix: conexión APks

--- app/src/main/java/com/example/spadelbosque/data/remote/ApiConfig.kt
+++ app/src/main/java/com/example/spadelbosque/data/remote/ApiConfig.kt
@@ -2,7 +2,7 @@ package com.example.spadelbosque.data.remote
 
 object ApiConfig {
     // APKs
-    const val BASE_URL = "http://192.168.1.13:8080/"
+    const val BASE_URL = "http://10.15.64.248:8080/"
 
     // Emulador
     //const val BASE_URL = "http://10.0.2.2:8080/"
